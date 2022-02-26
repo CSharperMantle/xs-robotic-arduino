1525ab762ba1ccac3b0598062185e0e1d936a603
Rong Mantle Bao <baorong2005@126.com>
2022-02-26 12:45:40 +0800

    traffic_light: add

---
+++ ArduinoIDESketches/morse/morse.ino
@@ -0,0 +1,27 @@
+/**
+ * Demonstrate the most simple usage: send one Morse Code message, once,
+ * while blocking (not doing anything else).
+ */
+
+#include <Arduino.h>
+#include <morse.h>
+
+#define PIN_STATUS  10
+
+static SpeakerMorseSender cqSender{PIN_STATUS};
+
+void setup()
+{
+  Serial.begin(115200);
+  cqSender.setup();
+}
+
+void loop()
+{
+  while (!Serial.available()) {
+    ;
+  }
+  String str = Serial.readStringUntil('$');
+  cqSender.setMessage(str);
+  cqSender.sendBlocking();
+}
---
+++ traffic_light_mbit/src/main.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+
+#define LIGHT_RED 0
+#define LIGHT_YELLOW 1
+#define LIGHT_GREEN 2
+
+#define PIN_LED_RED 12
+#define PIN_LED_YELLOW 11
+#define PIN_LED_GREEN 10
+
+void initLEDs() {
+  pinMode(PIN_LED_RED, OUTPUT);
+  pinMode(PIN_LED_YELLOW, OUTPUT);
+  pinMode(PIN_LED_GREEN, OUTPUT);
+}
+
+void setAllLEDs(int level) {
+  digitalWrite(PIN_LED_RED, level);
+  digitalWrite(PIN_LED_YELLOW, level);
+  digitalWrite(PIN_LED_GREEN, level);
+}
+
+int getPin(int light) {
+  switch (light) {
+  case LIGHT_RED:
+    return PIN_LED_RED;
+  case LIGHT_YELLOW:
+    return PIN_LED_YELLOW;
+  case LIGHT_GREEN:
+    return PIN_LED_GREEN;
+  default:
+    return -1;
+  }
+}
+
+void timedLightLED(int light, unsigned int duration) {
+  setAllLEDs(LOW);
+  digitalWrite(getPin(light), HIGH);
+  delay(duration);
+  setAllLEDs(LOW);
+}
+
+void pulseLEDs(int light,
+    unsigned int times,
+    unsigned int high_duration,
+    unsigned int low_duration) {
+  setAllLEDs(LOW);
+  for (unsigned int i = 0; i < times; i++) {
+    setAllLEDs(LOW);
+    delay(low_duration);
+    timedLightLED(light, high_duration);
+  }
+  setAllLEDs(LOW);
+}
+
+void setup() {
+  initLEDs();
+}
+
+void loop() {
+  timedLightLED(LIGHT_RED, 5000);
+  pulseLEDs(LIGHT_RED, 5, 500, 500);
+  timedLightLED(LIGHT_GREEN, 5000);
+  pulseLEDs(LIGHT_GREEN, 5, 500, 500);
+  timedLightLED(LIGHT_YELLOW, 2000);
+}
